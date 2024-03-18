dc5c512fda4058360d5099f6afefd4df74ce2755
Николай <123198970+IFilkov@users.noreply.github.com>
2024-03-18 15:50:59 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,76 +1,41 @@
-var tm;
-var cars;
-var cx;
-var gameBegin;
-const audio = document.querySelector('audio');
-audio.innerHTML = "audio/music.mp3";
+document.addEventListener("DOMContentLoaded", function () {
+  window.addEventListener("scroll", function () {
+    var aboutSection = document.getElementById("about").offsetTop;
+    var eventsSection = document.getElementById("events").offsetTop;
+    var jobsSection = document.getElementById("jobs").offsetTop;
+    var contactSection = document.getElementById("contacts").offsetTop;
+    var scrollPosition =
+      window.pageYOffset || document.documentElement.scrollTop;
 
-// var moveButton = document.querySelector(".move-btn");
-
-// var iSname = prompt("Как вас зовут?");
-// var summa = prompt("Введите вашу сумму");
-// var c = prompt("На какую машину (от 0 до 4) вы делаете ставку");
-// var stavka = prompt("Сколько вы ставите на выигрыш?");
-
-//   $('#remove').on('click', function(){
-//     $('.hoge').removeClass('js-move');
-//   });
-
-document.getElementById('p1').style.transform = 'rotateY(10deg)';
-document.getElementById('p2').style.transform = 'rotateY(10deg)';
-document.getElementById('p3').style.transform = 'rotateY(10deg)';
-document.getElementById('p4').style.transform = 'rotateY(10deg)';
-// document.getElementById('finish').style.marginLeft = '600px';
-
-// let wiew = VisualViewport;
-
-function go() {
-  document.getElementById("add").classList.remove("revers");
-  document.getElementById("add").classList.add("is-moved");
-  iddiv.innerHTML = "";
-  if (gameBegin == 1) return;
-  gameBegin = 1;
-  cars = [];
-  cx = [];
-  for (var i = 1; i < 5; i++) {
-    cars[i] = document.getElementById("p" + i);
-    cars[i].style.border = "none";
-    cx[i] = window.innerWidth; //1200
-  }
-
-  tm = setInterval(timerGo, 50);
-}
-
-function soundClick() {
-  var audio = new Audio(); // Создаём новый элемент Audio
-  audio.src = '/script.js' // Указываем путь к звуку "клика"
-  audio.autoplay = true; // Автоматически запускаем
-  
-}
-
-function timerGo() {
-  //   document.getElementById("add").onclick = function () {
-  //     document.getElementById("add").classList.add("is-moved");
-  //   };
-  for (var i = 1; i < 5; i++) {
-    cx[i] -= parseInt(Math.random() * 17 + 2);
-    if (cx[i] <= 40) {
-      clearInterval(tm);
-      gameBegin = 0;
-
-      cars[i].style.border = "5px ridge yellow";
-      document.getElementById("add").classList.remove("is-moved");
-      document.getElementById("add").classList.add("revers");
-      cars[i].style.transform = "translate";
-      console.log("Winner is " + i);
-      iddiv.innerHTML = "Winner is " + i;
-
-      return;
+    if (scrollPosition >= aboutSection && scrollPosition < eventsSection) {
+      document.querySelector(".overlay.about").classList.add("active");
+    } else {
+      document.querySelector(".overlay.about").classList.remove("active");
     }
-    // cars[i].style.left = cx[i] + "px";
-    cars[i].style.right = cx[i] + "px";
-  }
 
-  soundClick();
-  
-}
+    if (scrollPosition >= eventsSection && scrollPosition < jobsSection) {
+      document.querySelector(".overlay.events").classList.add("active");
+    } else {
+      document.querySelector(".overlay.events").classList.remove("active");
+    }
+
+    if (scrollPosition >= jobsSection && scrollPosition < contactSection) {
+      document.querySelector(".overlay.jobs").classList.add("active");
+    } else {
+      document.querySelector(".overlay.jobs").classList.remove("active");
+    }
+
+    if (scrollPosition >= contactSection) {
+      document.querySelector(".overlay.contact").classList.add("active");
+    } else {
+      document.querySelector(".overlay.contact").classList.remove("active");
+    }
+  });
+
+  const burgerMenu = document.querySelector(".burger-menu");
+  const menu = document.querySelector(".menu");
+
+  burgerMenu.addEventListener("click", function () {
+    menu.classList.toggle("active");
+  });
+});
